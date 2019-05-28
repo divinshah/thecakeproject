e6dea5db0074cdc48ea6b97b8b1a9b2909a7cd6d
Ekta <ektapatel71292@gmail.com>
2019-05-28 09:15:39 -0400

    Product detail page added

---
+++ productdetail.php
@@ -0,0 +1,42 @@
+<?php require "header.php" ?>
+<link rel="stylesheet" href="index.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<link rel="stylesheet" href="styles/productdetail.css">
+<body>
+
+  <div class="banner">
+    <img src="images/banner.jpg" alt="banner image">
+  </div>
+  
+  <section>
+    <p><img class="product" src="images/redvelvet.jpeg" alt="cake">
+      <h2>Red Velvet</h2>
+      <h3>$40</h3>
+      <select>
+        <option>Size</option>
+        <option>8"</option>
+        <option>12"</option>
+      </select><br/>
+      description of cake</p>
+
+	
+  </section>
+
+  <div class="container-fluid">
+  
+    <div class="row">
+    <div class="col-sm-4">img1</div>
+    <div class="col-sm-4">img2</div>
+    <div class="col-sm-4">img3</div>
+	</div>
+  
+</div>
+
+
+
+</body>
+
+<?php include "footer.php" ?>
