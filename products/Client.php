9481befc2c3025ff04ac96bb1223db4dac8e81b6
jetal <jetal@DESKTOP-FM7ARA9>
2019-05-28 08:33:06 -0400

    commited

---
+++ products/Client.php
@@ -0,0 +1,29 @@
+<?php
+class Client
+{
+	public function addClients($username,$email,$password,$db)
+	{
+		$sql = "INSERT INTO clients (user_name,email,password)
+		        VALUES (:user_name,:email,:password)";
+		$pst = $db->prepare($sql);
+
+		$pst->bindParam(':user_name', $username);
+        $pst->bindParam(':email', $email);
+        $pst->bindParam(':password', $password);
+
+        $count = $pst->execute();
+        return $count;
+
+	}
+
+	public function getAllClientsLogin($loginuser,$loginpass,$db){
+
+        $sql = "SELECT*FROM clients WHERE user_name='$loginuser' and password='$loginpass'";
+
+    $pst = $db->prepare($sql);
+    $pst->execute();
+    $Clients = $pst->fetchAll(PDO::FETCH_OBJ);
+
+    return $Clients;
+  }
+}
