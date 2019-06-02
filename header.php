383a2ae0ee0de8a617156c07d866bd0a45b21d79
jetal <jetal@DESKTOP-FM7ARA9>
2019-06-02 10:17:22 -0400

    changed some files

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="header.css">
+	<link rel="stylesheet" href="<?php echo ROOT; ?>/header.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<title>MR.Cake</title>
 	<nav>
 	<div class="handle">
-				<p class="menu"><img class="image_disp" src="images/logo.jpg" alt="cake logo"/></p>
+				<p class="menu"><img class="image_disp" src="<?php echo ROOT; ?>/images/logo.jpg" alt="cake logo"/></p>
 				<div class="menu_icon">
 			      <div></div>
 			      <div></div>
@@ -18,7 +18,7 @@
 				<a href="#"><li>Home</li></a>
 				<a href="#"><li>About</li></a>
 				<a href="#"><li>Cakes</li></a>
-				<img class="image" src="images/logo.jpg" alt="cake logo"/>
+				<img class="image" src=<?php echo ROOT; ?>/images/logo.jpg" alt="cake logo"/>
 				<a href="#"><li>Contact</li></a>
 				<a href="#"><li>FAQ</li></a>
 				<a href="login.php"><li>Login</li></a>
